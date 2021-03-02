a6d73e24b412623b9831fb71b7be9b9e9ed8ff8b
jcamiloangarita <camiloang94@gmail.com>
2021-03-02 09:08:20 +0100

    prepare new release: v0.3.6

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 setup(
     name='pwptemp',
     packages=['pwptemp'],
-    version='0.3.6b3',
+    version='0.3.6',
     license='LGPL v3',
     description='Well Temperature Distribution',
     long_description=long_description,
